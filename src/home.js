0bf4e4ee98f71b49496844a0e2993cdf710bde81
8BITeyes <RyanFrazin@Gmail.com>
2023-01-25 19:58:46 -0800

    Ryan Frazin Jan 25, 2023; 19:58
    Add nav to HTML
    
    Add functionality to nav on page;
    Add module load to nav bar on page;

--- src/home.js
+++ src/home.js
@@ -1,23 +1,23 @@
 import Rest from "./Rest.jpg";
 
-function component() {
+export default function component() {
   const element = document.createElement("div");
 
-  const title = document.createElement("h3");
+  const title = document.createElement("h1");
   title.innerHTML = "Rocco's Restaurant";
+  title.classList.add("title");
 
   const image = new Image();
   image.src = Rest;
 
   const review = document.createElement("div");
   review.innerHTML = `"Wow. What a Restaurant." ~ Rocco Botte`;
+  review.classList.add("review");
 
   element.appendChild(title);
   element.appendChild(image);
   element.appendChild(review);
 
-  return element;
+  const node = document.getElementById("content");
+  node.appendChild(element);
 }
-
-let restPage = document.querySelector("div#content");
-restPage.appendChild(component());
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import "./module";
+import component from "./home";
+import "./style.css";
+
+const menuBody = document.querySelector("div#content");
+
+const homeButton = document.querySelector("#home");
+homeButton.addEventListener("click", function () {
+  component();
+});
